7b9d0927bcd132d10307ee1ceaab9d9dd58175a7
Bianca van Schaik <bvschaik@gmail.com>
2018-07-09 20:33:04 +0200

    Move mappers to separate file

--- app/src/main/java/nl/biancavanschaik/android/museumkaart/data/MuseumDetailsRepository.kt
+++ app/src/main/java/nl/biancavanschaik/android/museumkaart/data/MuseumDetailsRepository.kt
@@ -5,12 +5,9 @@ import nl.biancavanschaik.android.museumkaart.data.database.MuseumDao
 import nl.biancavanschaik.android.museumkaart.data.database.model.Listing
 import nl.biancavanschaik.android.museumkaart.data.database.model.Museum
 import nl.biancavanschaik.android.museumkaart.data.rest.MuseumRestService
-import nl.biancavanschaik.android.museumkaart.data.rest.model.Exhibition
-import nl.biancavanschaik.android.museumkaart.data.rest.model.Listings
 import nl.biancavanschaik.android.museumkaart.data.rest.model.MuseumDetails
 import nl.biancavanschaik.android.museumkaart.util.IsoDate
 import nl.biancavanschaik.android.museumkaart.util.Resource
-import nl.biancavanschaik.android.museumkaart.data.database.model.MuseumDetails as DbMuseumDetails
 
 class MuseumDetailsRepository(
         private val museumRestService: MuseumRestService,
@@ -44,50 +41,4 @@ class MuseumDetailsRepository(
         // only use the database cache: we cannot get here without having fetched details
         return museumDao.findListingById(listingId)
     }
-
-    private fun MuseumDetails.toDatabaseObject(cacheItem: Museum?): Museum {
-        val details = DbMuseumDetails(
-                id = permanentid,
-                numericId = id,
-                displayName = displayname,
-                address = streetandnumber,
-                city = city,
-                telephone = telephone,
-                website = website,
-                email = email,
-                imagePath = path,
-                admissionPrice = admissionprice,
-                openingHours = openinghours,
-                museumCardParticipant = museumcardparticipant,
-                lat = lat,
-                lon = lon,
-                dateFetched = IsoDate.today(),
-                visitedOn = cacheItem?.details?.visitedOn,
-                wishList = cacheItem?.details?.wishList == true
-        )
-        return Museum(details, listings.toDatabaseObject(details).distinctBy { it.id })
-    }
-
-    private fun Listings.toDatabaseObject(museum: DbMuseumDetails) =
-            permanent.map { it.toDatabaseObject(museum, Listing.Type.PERMANENT) } +
-                    exhibition.map { it.toDatabaseObject(museum, Listing.Type.EXHIBITION) } +
-                    promotion.map { it.toDatabaseObject(museum, Listing.Type.PROMOTION) } +
-                    event.map { it.toDatabaseObject(museum, Listing.Type.EVENT) }
-
-    private fun Exhibition.toDatabaseObject(museum: DbMuseumDetails, type: Listing.Type) =
-            Listing(
-                    id = permanentid,
-                    numericId = id,
-                    museumId = museum.id,
-                    type = type,
-                    startDate = IsoDate.fromIsoString(startdate),
-                    endDate = IsoDate.fromIsoString(enddate),
-                    name = name,
-                    locationId = locationid,
-                    description = description,
-                    openingHours = openinghours,
-                    admissionPrice = admissionprice,
-                    imagePath = path,
-                    city = city
-            )
 }
